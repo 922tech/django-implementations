8510cd54efb822505a369ec8de84dace21db3bb0
922tech <108605086+922tech@users.noreply.github.com>
2023-08-02 14:24:43 +0330

    Update urls.py

--- djangoProject4/urls.py
+++ djangoProject4/urls.py
@@ -7,12 +7,13 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include('home.urls'), name='home'),
+    path('shop/', include('shop.urls'), name='shop'),
     path('blog/', include('weblog.urls'), name='blog'),
     path('ui/', include('ui.urls'), name='shop'),
+    # path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     # path("__reload__/", include("django_browser_reload.urls")),
 ]
 if settings.DEBUG:
